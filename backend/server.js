19317a7f9d161a33a92de3196d50954da24a5664
ckpaz12 <little_claris2@yahoo.com>
2024-03-28 07:42:30 -0700

    server setup files

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const http = require('http');
-
-const app = http.createServer();
-
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`App is running on port ${PORT}`);
 });
