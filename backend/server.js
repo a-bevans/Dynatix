9467df226a9f3a5d640bc38163afdde4bc948976
ckpaz12 <little_claris2@yahoo.com>
2024-03-28 08:22:08 -0700

    database setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,3 +11,20 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
 });
+
+const { MongoClient } = require('mongodb');
+
+const password = process.env.PASSWORD;
+const dbName = 'dynatix-db';
+const uri = `mongodb+srv://dynatix:${password}@dynatix-db.mkuq59f.mongodb.net/?retryWrites=true&w=majority&appName=${dbName}`;
+const client = new MongoClient(uri);
+var db = null;
+
+/* MongoDB Documentation http://mongodb.github.io/node-mongodb-native/4.2/ */
+async function connectToDB() {
+    await client.connect();
+    console.log('Sucessful Connection to DB');
+    db = client.db(dbName);
+}
+
+connectToDB();
