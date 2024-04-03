2466958e6e12ed6676770022149b9840dc18d6bf
noodnoof <nadianafeesa@gmail.com>
2024-04-03 14:39:38 -0700

    settled phone number check with database

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyparser.json());
 const corsOptions = {
-    origin: "http://localhost:5500",
+    origin: "http://127.0.0.1:5500",
     credentials: true,
 };
 app.use(cors(corsOptions));
